c50ded4ff2ea1fd47862a00fc4e736bd456e3082
chris.budde <chris.budde@northerntool.com>
2020-03-04 13:12:21 -0600

    updated browsers.ps1

--- scripts/Browsers.ps1
+++ scripts/Browsers.ps1
@@ -1,3 +1,9 @@
 #--- Browsers ---
+Write-Host ""
+Write-Host "Installing Google Chrome..." -ForegroundColor Green
+Write-Host "------------------------------------" -ForegroundColor "Green"
 choco install -y googlechrome
+Write-Host ""
+Write-Host "Installing FireFox..." -ForegroundColor Green
+Write-Host "------------------------------------" -ForegroundColor "Green"
 choco install -y firefox
