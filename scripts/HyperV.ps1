ae7f6ddf2f8385206e70d037bc43ea963e525f55
chris.budde <chris.budde@northerntool.com>
2020-03-05 11:40:50 -0600

    Update HyperV.ps1

--- scripts/HyperV.ps1
+++ scripts/HyperV.ps1
@@ -1,4 +1,5 @@
 Write-Host ""
 Write-Host "Installing HyperV..." -ForegroundColor Green
 Write-Host "------------------------------------" -ForegroundColor "Green"
-choco install -y Microsoft-Hyper-V-All --source="'windowsFeatures'"
+#choco install -y Microsoft-Hyper-V-All --source="'windowsFeatures'"
+choco windowsfeatures Microsoft-Hyper-V-All
